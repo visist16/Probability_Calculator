850a3ca4be537c696ba628a56e241b647a7b08fd
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-07-25 22:09:15 +0530

    Update prob_calculator.py

--- prob_calculator.py
+++ prob_calculator.py
@@ -27,11 +27,4 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     hat_copy = copy.deepcopy(hat)
     colors_gotten = hat_copy.draw(num_balls_drawn)
 
-    for color in colors_gotten:
-      if(color in expected_copy):
-        expected_copy[color]-=1
-
-    if(all(x <= 0 for x in expected_copy.values())):
-      count += 1
-
-  return count / num_experiments
+   
